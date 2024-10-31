a31dddda6f5b317c79589a92f7c7dbe7af00e859
deadonted <timofejmitusin60892@gmail.com>
2024-11-01 00:42:27 +0300

    feat: add models.py

---
+++ app/app/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship, DeclarativeBase
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+	pass
+
+
+class User(Base):
+	__tablename__ = 'users'
+	id = Column(Integer, primary_key=True, index=True)
+	username = Column(String(20), unique=True, nullable=False, index=True)
+	password_hash = Column(String(100), nullable=False)
+
+	tasks = relationship('Task', back_populates='owner')
+
+
+class Task(Base):
+	__tablename__ = 'tasks'
+	id = Column(Integer, primary_key=True, index=True)
+	task_name = Column(String(100), nullable=False)
+	description = Column(String(500), nullable=False)
+	status = Column(String(50), nullable=False)
+	user_id = Column(Integer, ForeignKey('users.id'), index=True)
+
+	owner = relationship('User', back_populates='tasks')
