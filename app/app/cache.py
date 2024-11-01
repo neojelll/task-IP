501dc36f5eecb4ba05a483834e1a44737a1f67e6
deadonted <timofejmitusin60892@gmail.com>
2024-11-01 11:57:04 +0300

    fix: cache.py

--- app/app/cache.py
+++ app/app/cache.py
@@ -1,6 +1,5 @@
 from redis.asyncio import Redis
 from .logger import configure_logger
-from loguru import logger
 import os
 
 
@@ -8,21 +7,21 @@ configure_logger()
 
 
 class Cache:
-	def __init__(self):
-		self.session = Redis(
-			host=os.environ["CACHE_HOST"],
+    def __init__(self):
+        self.session = Redis(
+            host=os.environ["CACHE_HOST"],
             port=int(os.environ["CACHE_PORT"]),
             decode_responses=True,
-		)
+        )
 
-	async def __aenter__(self):
-		return self
-	
-	async def create_recording(self, username, refresh_token: str):
-		await self.session.set(refresh_token, username, ex=3600)
+    async def __aenter__(self):
+        return self
 
-	async def check_recording(self, refresh_token):
-		return await self.session.get(refresh_token)
-	
-	async def __aexit__(self, exc_type, exc_value, traceback):
-		await self.session.aclose()
+    async def create_recording(self, username, refresh_token: str):
+        await self.session.set(refresh_token, username, ex=3600)
+
+    async def check_recording(self, refresh_token):
+        return await self.session.get(refresh_token)
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.session.aclose()
