6dc9b8f797018ef3c99c169fb573ba47bee323c1
deadonted <timofejmitusin60892@gmail.com>
2024-11-01 11:57:47 +0300

    refactor: models.py

--- app/app/models.py
+++ app/app/models.py
@@ -4,24 +4,24 @@ from sqlalchemy.orm import relationship, DeclarativeBase
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-	pass
+    pass
 
 
 class User(Base):
-	__tablename__ = 'users'
-	id = Column(Integer, primary_key=True, index=True)
-	username = Column(String(20), unique=True, nullable=False, index=True)
-	password_hash = Column(String(100), nullable=False)
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(20), unique=True, nullable=False, index=True)
+    password_hash = Column(String(100), nullable=False)
 
-	tasks = relationship('Task', back_populates='owner')
+    tasks = relationship("Task", back_populates="owner")
 
 
 class Task(Base):
-	__tablename__ = 'tasks'
-	id = Column(Integer, primary_key=True, index=True)
-	task_name = Column(String(100), nullable=False)
-	description = Column(String(500), nullable=False)
-	status = Column(String(50), nullable=False)
-	user_id = Column(Integer, ForeignKey('users.id'), index=True)
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    task_name = Column(String(100), nullable=False)
+    description = Column(String(500), nullable=False)
+    status = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), index=True)
 
-	owner = relationship('User', back_populates='tasks')
+    owner = relationship("User", back_populates="tasks")
