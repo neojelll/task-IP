ee3ba5b63903f3f7758d351d6478e0f97be38016
deadonted <timofejmitusin60892@gmail.com>
2024-11-01 16:26:47 +0300

    fix: cache.py

--- app/app/cache.py
+++ app/app/cache.py
@@ -1,5 +1,6 @@
 from redis.asyncio import Redis
 from .logger import configure_logger
+from loguru import logger
 import os
 
 
@@ -18,10 +19,16 @@ class Cache:
         return self
 
     async def create_recording(self, username, refresh_token: str):
-        await self.session.set(refresh_token, username, ex=3600)
+        try:
+            await self.session.set(refresh_token, username, ex=3600 * 24 * 7)
+        except Exception as e:
+            logger.error(f"Error when set in cache: {e}")
 
     async def check_recording(self, refresh_token):
-        return await self.session.get(refresh_token)
+        try:
+            return await self.session.get(refresh_token)
+        except Exception as e:
+            logger.error(f"Error when get in cache: {e}")
 
     async def __aexit__(self, exc_type, exc_value, traceback):
         await self.session.aclose()
